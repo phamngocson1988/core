17f45ac668eaf103824be68abd5c7f14d583b538
Son Pham <phamngocson1988@gmail.com>
2018-11-02 00:57:55 +0700

    add more feature to gridview

--- backend/components/gridview/ActionColumn.php
+++ backend/components/gridview/ActionColumn.php
@@ -28,5 +28,62 @@ use yii\helpers\Url;
  */
 class ActionColumn extends BaseActionColumn
 {
-	
+	// <a href="/category/view?id=1" title="Xem" aria-label="Xem" data-pjax="0"><span class="glyphicon glyphicon-eye-open"></span></a>
+	// <a href="http://admin.chuchu.com:8080/game/edit?id=1&amp;ref=http%3A%2F%2Fadmin.chuchu.com%3A8080%2Fgame" class="btn btn-xs grey-salsa tooltips" data-container="body" data-original-title="Chỉnh sửa"><i class="fa fa-pencil"></i></a>
+
+	// protected function initDefaultButtons()
+ //    {
+	// 	$pattern = '/\\{([\w\-\/]+)\\}/';
+	// 	preg_match_all($pattern, $this->template, $matches);
+	// 	$buttons = ArrayHelper::getValue($matches, '1', []);
+ //    	foreach ($this->buttons as $button => $buttonOptions) {
+ //    		// $func = sprintf("init%sButton", ucfirst($button));
+ //    		// $this->buttons[$button] = $this->$func($options);
+ //    		$title = Yii::t('app', $button);
+ //    		$options = array_merge([
+ //                'title' => $title,
+ //                'aria-label' => $title,
+ //                'data-pjax' => '0',
+ //            ], $buttonOptions);
+ //            $icon = Html::tag('i', '', ['class' => "fa fa-pencil"]);
+ //            $this->buttons[$button] = Html::a($icon, '#', $options);
+ //    	}
+ //    }
+
+	protected function initDefaultButton($name, $iconName, $additionalOptions = [])
+    {
+        if (!isset($this->buttons[$name]) && strpos($this->template, '{' . $name . '}') !== false) {
+            $this->buttons[$name] = function ($url, $model, $key) use ($name, $iconName, $additionalOptions) {
+                switch ($name) {
+                    case 'view':
+                        $title = Yii::t('yii', 'View');
+                        $iconClass = "fa fa-eye";
+                        break;
+                    case 'update':
+                        $title = Yii::t('yii', 'Update');
+                        $iconClass = "fa fa-pencil";
+                        break;
+                    case 'delete':
+                        $title = Yii::t('yii', 'Delete');
+                        $iconClass = "fa fa-trash-o";
+                        break;
+                    default:
+                        $title = ucfirst($name);
+                        $iconClass = "";
+                }
+                $additionalOptions = [
+                	'class' => 'btn btn-xs grey-salsa tooltips', 
+                	'data-container' => 'body', 
+                	'data-original-title' => $title
+                ];
+                $options = array_merge([
+                    'title' => $title,
+                    'aria-label' => $title,
+                    'data-pjax' => '0',
+                ], $additionalOptions, $this->buttonOptions);
+                $icon = Html::tag('i', '', ['class' => $iconClass]);
+                return Html::a($icon, $url, $options);
+            };
+        }
+    }
 }
