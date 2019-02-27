629efb0a00c767b3bed877560e7bf739aa547dfe
Ngoc Son <phamngocson1988@gmail.com>
2019-02-27 16:59:31 +0700

    add to cart

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -277,4 +277,64 @@ class SiteController extends Controller
             'model' => $model,
         ]);
     }
+
+    public function actionTest()
+    {
+        // 2. Provide your Secret Key. Replace the given one with your app clientId, and Secret
+        // https://developer.paypal.com/webapps/developer/applications/myapps
+        $apiContext = new \PayPal\Rest\ApiContext(
+            new \PayPal\Auth\OAuthTokenCredential(
+                // 'AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS',     // ClientID
+                // 'EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL'      // ClientSecret
+
+                'AQK-NCCq492D7OEICMTiFzyWPskls32NEhwZ9t7eERBk2kHuhjywMFA8BjMkj1XqFvQTtok6Srs1R-OF',     // ClientID
+                'EBmAgMX7piQWJu1gkuCbmIRW3MJ1pv-cdYbsxmKj6-esCGhGwCoQ4e-eoQu0d7MCHJxrMKSlY81RFvjx'      // ClientSecret
+            )
+        );
+
+        // Step 2.1 : Between Step 2 and Step 3
+        $apiContext->setConfig(
+            array(
+              'log.LogEnabled' => true,
+              'log.FileName' => 'PayPal.log',
+              'log.LogLevel' => 'DEBUG'
+            )
+        );
+
+        // 3. Lets try to create a Payment
+        // https://developer.paypal.com/docs/api/payments/#payment_create
+        $payer = new \PayPal\Api\Payer();
+        $payer->setPaymentMethod('paypal');
+
+        $amount = new \PayPal\Api\Amount();
+        $amount->setTotal('123');
+        $amount->setCurrency('USD');
+
+        $transaction = new \PayPal\Api\Transaction();
+        $transaction->setAmount($amount);
+
+        $redirectUrls = new \PayPal\Api\RedirectUrls();
+        $redirectUrls->setReturnUrl("http://kinggerm.com/site/success.html")
+            ->setCancelUrl("http://kinggerm.com/site/error.html");
+
+        $payment = new \PayPal\Api\Payment();
+        $payment->setIntent('sale')
+            ->setPayer($payer)
+            ->setTransactions(array($transaction))
+            ->setRedirectUrls($redirectUrls);
+
+        // 4. Make a Create Call and print the values
+        try {
+            $payment->create($apiContext);
+            echo $payment;
+
+            echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
+            return $this->redirect($payment->getApprovalLink());
+        }
+        catch (\PayPal\Exception\PayPalConnectionException $ex) {
+            // This will print the detailed information on the exception.
+            //REALLY HELPFUL FOR DEBUGGING
+            echo $ex->getData();
+        }
+    }
 }
