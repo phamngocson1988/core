6e58ecd81174ff5fca757e765f1705a8abddf871
Son Pham <phamngocson1988@gmail.com>
2019-11-03 01:06:28 +0700

    fix

--- backend/views/order/verifying.php
+++ backend/views/order/verifying.php
@@ -207,6 +207,29 @@ use backend\components\datetimepicker\DateTimePicker;
                   <a href='<?=$model->evidence;?>' class="btn btn-xs grey-salsa tooltips" data-pjax="0" data-container="body" data-original-title="Xem hóa đơn" target="_blank"><i class="fa fa-file"></i></a>
                   <?php endif;?>
                   <a href='<?=Url::to(['order/send-mail-verifying-order', 'id' => $model->id]);?>' class="btn btn-xs grey-salsa tooltips send-mail" data-pjax="0" data-container="body" data-original-title="Send mail"><i class="fa fa-envelope"></i></a>
+
+                  <a href='#go_pending<?=$model->id;?>' class="btn btn-xs grey-salsa tooltips approve" data-pjax="0" data-container="body" data-original-title="Send mail" data-toggle="modal" ><i class="fa fa-check"></i></a>
+                  <div class="modal fade" id="go_pending<?=$model->id;?>" tabindex="-1" role="basic" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                          <h4 class="modal-title">Chuyển tới trạng thái Pending</h4>
+                        </div>
+                        <?php $nextForm = ActiveForm::begin(['options' => ['class' => 'form-row-seperated move-to-pending', 'order-id' => $model->id], 'action' => Url::to(['order/move-to-pending', 'id' => $model->id])]);?>
+                        <div class="modal-body"> 
+                          <p>Bạn có chắc chắn muốn chuyển đơn hàng này sang trạng thái "Pending". Hãy chắc chắn rằng đơn hàng này đã được thanh toán</p>
+                          <?=$nextForm->field($model, 'payment_method', ['inputOptions' => ['class' => 'form-control', 'id' => 'payment_method' . $model->id]])->textInput();?>
+                          <?=$nextForm->field($model, 'payment_id', ['inputOptions' => ['class' => 'form-control', 'id' => 'payment_id' . $model->id]])->textInput();?>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
+                          <button type="submit" class="btn green">Xác nhận</button>
+                        </div>
+                        <?php ActiveForm::end();?>
+                      </div>
+                    </div>
+                  </div>
                 </td>
               </tr>
               <?php endforeach;?>
@@ -246,6 +269,26 @@ $('.send-mail').ajax_action({
     location.reload();
   }
 });
+
+// move to pending
+var executeForm = new AjaxFormSubmit({element: '.move-to-pending'});
+executeForm.success = function (data, form) {
+  location.reload();
+};
+
+executeForm.validate = function(form) {
+  var id = $(form).attr('order-id');
+  var payment_method = $.trim($(form).find('#payment_method' + id).val());
+  var payment_id = $.trim($(form).find('#payment_id' + id).val());
+  if (payment_method == '' || payment_id == '') {
+    return false;
+  }
+  return true;
+}
+
+executeForm.invalid = function(form) {
+  alert('Nội dung không được để trống');
+}
 JS;
 $this->registerJs($script);
 ?>
