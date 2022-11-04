a6a735dc471e94d892967c575082cef4bc90fe9d
Son Pham <son.pn@c-plus.cloud>
2022-11-04 10:09:22 +0700

    improve commission report

--- backend/controllers/ReportCommissionController.php
+++ backend/controllers/ReportCommissionController.php
@@ -46,6 +46,25 @@ class ReportCommissionController extends Controller
         return $this->render('index');
     }
 
+    public function actionDetail()
+    {
+        $this->view->params['main_menu_active'] = 'report.commission.index';
+        $request = Yii::$app->request;
+        $type = $request->get('type');
+
+        $form = new \backend\forms\ReportCommissionForm([
+            'user_ids' => [$request->get('user_id')],
+            'start_date' => $request->get('start_date'),
+            'end_date' => $request->get('end_date'),
+        ]);
+        $form->run();
+        $data = $form->getData();
+        $data = array_filter($data, function($row) use ($type) {
+            return $row['commission_type'] === $type;
+        });
+        return $this->render('detail', ['data' => $data]);
+    }
+
     public function actionOrderDetail($id, $type, $role)
     {
         $order = \backend\models\Order::findOne($id);
