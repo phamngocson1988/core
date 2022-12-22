8d886be0389c5cab26d24a3dfb8ece7306f34da7
Son Pham <phamngocson1988@gmail.com>
2022-12-23 02:03:16 +0700

    add exporting function

--- backend/forms/ReportCommissionForm.php
+++ backend/forms/ReportCommissionForm.php
@@ -8,6 +8,7 @@ use yii\helpers\ArrayHelper;
 use backend\models\OrderCommission;
 use backend\models\User;
 use backend\models\Game;
+use common\components\helpers\StringHelper;
 
 class ReportCommissionForm extends Model
 {
@@ -126,5 +127,123 @@ class ReportCommissionForm extends Model
         }
 
         return $result;
+    }
+
+    public function export($fileName = null)
+    {
+        $dataByUser = $this->getCommissionByUser();
+        $fileName = ($fileName) ? $fileName : 'order-commission' . date('His') . '.xlsx';
+        $names = [
+            'Nhân viên',
+            'Sell out',
+            'Hoa hồng',
+            'Tổng',
+        ];
+        $characters = [ 'A', 'B', 'C', 'D' ];
+        $titles = array_combine($characters, $names);
+        $totalRow = count($dataByUser);
+        $startRow = 6;
+        $endRow = $startRow + $totalRow;
+        $footerRow = $endRow + 1;
+        $columns = array_keys($titles);
+        $startColumn = reset($columns);
+        $endColumn = end($columns);
+
+        $rangeTitle = sprintf('%s%s:%s%s', $startColumn, $startRow, $endColumn, $startRow);
+        $rangeData = sprintf('%s%s:%s%s', $startColumn, $startRow + 1, $endColumn, $endRow);
+        $rangeTable = sprintf('%s%s:%s%s', $startColumn, $startRow, $endColumn, $endRow + 1);
+
+        $users = $this->fetchUsers();
+        if ($this->user_ids && count($this->user_ids)) {
+            $users = array_intersect_key(
+                $users,  // the array with all keys
+                array_flip($this->user_ids) // keys to be extracted
+            );
+        }
+        $games = $this->fetchGames();
+        if ($this->game_ids && count($this->game_ids)) {
+            $games = array_intersect_key(
+                $games,  // the array with all keys
+                array_flip($this->game_ids) // keys to be extracted
+            );
+        }
+
+        $header = [
+            "A1:{$endColumn}1" => sprintf('THỐNG KÊ HOA HỒNG NHÂN VIÊN'),
+            "A2:{$endColumn}2" => sprintf('Thời gian thống kê: %s đến %s', $this->start_date, $this->end_date),
+            "A3:{$endColumn}3" => sprintf('Nhân viên: %s', implode(", ", $users)),
+            "A4:{$endColumn}4" => sprintf('Games: %s', implode(", ", $games)),
+        ];
+
+        $commissions = $this->getData();
+        $orderIds = ArrayHelper::getColumn($commissions, 'order_id');
+        $orderIds = array_unique($orderIds);
+        $sumSelloutCommission = array_sum(ArrayHelper::getColumn($dataByUser, OrderCommission::COMMSSION_TYPE_SELLOUT));
+        $sumOrderCommission = array_sum(ArrayHelper::getColumn($dataByUser, OrderCommission::COMMSSION_TYPE_ORDER));
+        $footer = [
+            "A$footerRow" => sprintf('Total Orders: %s', count($orderIds)),
+            "B$footerRow" => StringHelper::numberFormat($sumSelloutCommission, 0),
+            "C$footerRow" => StringHelper::numberFormat($sumOrderCommission, 0),
+            "D$footerRow" => ''
+        ];
+        
+        $data = [];
+        foreach ($dataByUser as $userId => $commission) {
+            $data[] = [
+                $commission['name'],
+                StringHelper::numberFormat($commission[OrderCommission::COMMSSION_TYPE_SELLOUT], 0),
+                StringHelper::numberFormat($commission[OrderCommission::COMMSSION_TYPE_ORDER], 0),
+                StringHelper::numberFormat($commission[OrderCommission::COMMSSION_TYPE_SELLOUT] + $commission[OrderCommission::COMMSSION_TYPE_ORDER], 0)
+            ];
+        }
+        $file = \Yii::createObject([
+            'class' => 'codemix\excelexport\ExcelFile',
+            'writerClass' => '\PHPExcel_Writer_Excel5', //\PHPExcel_Writer_Excel2007
+            'sheets' => [
+                'Report by transaction' => [
+                    'class' => 'common\components\export\excel\ExcelSheet',//'codemix\excelexport\ExcelSheet',
+                    // 'heading' => $heading,
+                    'header' => $header,
+                    'footer' => $footer,
+                    'data' => $data,
+                    'startRow' => $startRow,
+                    'titles' => $titles,
+                    'styles' => [
+                        $rangeTitle => [
+                            'font' => [
+                                'bold' => true,
+                            ],
+                            'alignment' => [
+                                'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                            ],
+                        ],
+                        $rangeTable => [
+                            'borders' => array(
+                                'allborders' => array(
+                                    'style' => \PHPExcel_Style_Border::BORDER_THIN
+                                )
+                            )
+                        ],
+                    ],
+                    
+                    'on beforeRender' => function ($event) {
+                        $sender = $event->sender;
+                        $sheet = $sender->getSheet();
+                        $sender->renderHeader();
+                        $sender->renderFooter();
+                        $titles = $sender->getTitles();
+                        $columns = array_keys($titles);
+                        foreach ($columns as $column) {
+                            $sheet->getColumnDimension($column)->setAutoSize(true);
+                        }
+                    },
+                    'on afterRender' => function($event) {
+                        $sheet = $event->sender->getSheet();
+                        $sheet->setSelectedCell("A1");
+                    }
+                ],
+            ],
+        ]);
+        $file->send($fileName);
     }
 }
