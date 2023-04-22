f6beae3e5b2533b861100f88026539a618a59651
Son Pham <phamngocson1988@gmail.com>
2023-04-22 14:59:05 +0700

    add datetime filter to affiliate

--- website/widgets/views/affiliate_chart.php
+++ website/widgets/views/affiliate_chart.php
@@ -1,1 +1,14 @@
+<div class="d-flex ml-auto">
+  <div class="flex-fill d-flex align-items-center mr-3 field-chart-from-date">
+    <label class="d-block w-100 mr-2 mb-0" for="chart-from-date">Start date</label>
+    <input type="date" id="chart-from-date" class="form-control" data-date-format="DD MMMM YYYY">
+  </div>
+  <div class="flex-fill d-flex align-items-center mr-3 field-chart-to-date">
+    <label class="d-block w-100 mr-2 mb-0" for="chart-to-date">End date</label>
+    <input type="date" id="chart-to-date" class="form-control">
+  </div>
+  <div class="flex-fill">
+    <button class="btn btn-primary" type="submit">Filter</button>
+  </div>
+</div>        
 <canvas id="myChart"></canvas>
